184cfb0c706e691f22dad7a3d752e3ecae8b1abc
Syed Osama Ahmed <batch17.026@gmail.com>
2023-08-10 12:25:30 +0500

    set posts feature added displaying feeds completed

--- lib/common/post_add_card.dart
+++ lib/common/post_add_card.dart
@@ -1,9 +1,13 @@
+import 'package:any_link_preview/any_link_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/Features/auth/controllers/auth_controller.dart';
 import 'package:reddit_clone/models/post.dart';
+import 'package:reddit_clone/theme/pallete.dart';
 
 class PostCard extends ConsumerWidget {
   final Post post;
+
   const PostCard({
     super.key,
     required this.post,
@@ -11,6 +15,133 @@ class PostCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Text(post.communityName);
+    final isTypeImage = post.type == 'image';
+    final isTypeText = post.type == 'text';
+    final isTypeLink = post.type == 'link';
+
+    final currentTheme = ref.watch(themeNotifierProvider);
+    final user = ref.watch(userProvider)!;
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: currentTheme.drawerTheme.backgroundColor,
+          ),
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 4,
+                        horizontal: 16,
+                      ).copyWith(
+                        right: 0,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                      post.communityProfilePic,
+                                    ),
+                                    radius: 16,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                      left: 8,
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          'r/${post.communityName}',
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          'u/${post.username}',
+                                          style: const TextStyle(
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              if (post.uid == user.uid)
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: Pallete.redColor,
+                                    )),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 18.0),
+                            child: Text(
+                              post.title,
+                              style: const TextStyle(
+                                fontSize: 19,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          if (isTypeImage)
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.35,
+                              width: double.infinity,
+                              child: Image.network(
+                                post.link!,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          if (isTypeLink)
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.35,
+                              width: double.infinity,
+                              child: AnyLinkPreview(
+                                displayDirection:
+                                    UIDirection.uiDirectionHorizontal,
+                                link: post.link!,
+                              ),
+                            ),
+                          if (isTypeText)
+                            Container(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 15.0,
+                                ),
+                                child: Text(
+                                  post.description!,
+                                  style: const TextStyle(color: Colors.grey),
+                                ),
+                              ),
+                            )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
